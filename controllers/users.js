58613bf8a1e2a97bcc52b6b628ebfa1579fb8681
Anna Kesova <IZABELE-TAN4@yandex.ru>
2022-08-03 21:35:31 +0300

    Проектная работа.13.tests0/8

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const {
   ERROR_CODE,
   ERROR_NOT_FOUND,
   ERROR_DEFAULT,
-  SUCCESS_STATUS,
+  //SUCCESS_STATUS,
 } = require('../utils/utils');
 
 module.exports.getUser = (req, res) => {
@@ -36,7 +36,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => {
-      res.status(SUCCESS_STATUS);
+      //res.status(SUCCESS_STATUS);
       res.send(user);
     })
     .catch((err) => {
