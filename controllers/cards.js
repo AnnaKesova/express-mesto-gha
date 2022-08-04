629d9f64af891a9b6f695dd28ba3e9d750c280d8
Anna Kesova <IZABELE-TAN4@yandex.ru>
2022-08-04 08:52:48 +0300

    Проектная работа.13.tests0/22.2

--- controllers/cards.js
+++ controllers/cards.js
@@ -36,13 +36,16 @@ module.exports.createCard = (req, res) => {
 module.exports.deleteCard = (req, res) => {
   const { id } = req.params.cardId;
   Card.findByIdAndRemove(id)
+    .orFail(new Error('NonExistentCard'))
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(ERROR_CODE.status).send({ message: ERROR_CODE.message });
-        return;
-      }
-      res.status(ERROR_DEFAULT.status).send({ message: ERROR_DEFAULT.message });
+      if (err.statusCode === ERROR_NOT_FOUND) {
+        res.status(ERROR_NOT_FOUND.status)
+          .send({ message: ERROR_NOT_FOUND.message });
+      } else if (err.name === 'CastError') {
+        res.status(ERROR_CODE.status)
+          .send({ message: ERROR_CODE.message });
+      } else { res.status(ERROR_DEFAULT.status).send({ message: ERROR_DEFAULT.message }); }
     });
 };
 
