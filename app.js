88d12f54932d34ad5d756af9babeb1695a021810
Anna Kesova <IZABELE-TAN4@yandex.ru>
2022-08-14 23:00:10 +0300

    Проектная работа.14. 0/16

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().regex(/^(https|http)?:\/\/(www.)?[\w-._~:/?#[\]@!$&'()*+,;=]*#?/),
   }),
 }), createUser);
 // авторизация
